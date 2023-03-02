8c72e4b94fa69374fa99a5363cbf3d61e9a31e2d
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2023-03-02 16:54:27 +0200

    added styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import TweetCard from './TweetCard';
 import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    body: 'Montserrat, sans-serif',
+  },
+  colors: {
+    cardButton: {
+      follow: '#EBD8FF',
+      following: '#5CD3A8',
+    },
+  },
+});
 
 export const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <TweetCard />
     </ChakraProvider>
   );
--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import logo from '../../media/Logo.svg';
+import bg from '../../media/Bg.svg';
 import {
   Box,
   Text,
@@ -18,9 +20,8 @@ const TweetCard = ({ userId = 1 }) => {
     return user || {};
   });
   const [followers, setFollowers] = useState(() => {
-    const storedData = localStorage.getItem('usersData');
-    const addStoredData = JSON.parse(storedData) || [];
-    const user = addStoredData.find(user => user.id === userId);
+    const storedData = JSON.parse(localStorage.getItem('usersData')) || [];
+    const user = storedData.find(user => user.id === userId);
     return user ? user.followers : userData.followers || 0;
   });
 
@@ -55,6 +56,40 @@ const TweetCard = ({ userId = 1 }) => {
   const cardBgColor = useColorModeValue('#471CA9', 'gray.700');
   const cardBorderColor = useColorModeValue('gray.200', 'gray.600');
 
+  const basicTweetCardStyles = {
+    fontWeight: '500',
+    fontSize: '20px',
+    lineHeight: '1.2',
+    textTransform: 'uppercase',
+  };
+
+  const stripe = {
+    pos: 'absolute',
+    w: '379px',
+    h: '8px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bg: '#EBD8FF',
+    boxShadow:
+      '0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06), inset 0px -1.71846px 3.43693px #AE7BE3, inset 0px 3.43693px 2.5777px #FBF8FF',
+  };
+
+  const avatarStyles = {
+    pos: 'absolute',
+    w: '80px',
+    h: '80px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bg: '#471CA9',
+    border: '8px solid #EBD8FF',
+    padding: '8px',
+    boxShadow:
+      '0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06), inset 0px -1.71846px 3.43693px #AE7BE3, inset 0px 3.43693px 2.5777px #FBF8FF',
+    borderRadius: '50%',
+  };
+
   return (
     <Box
       w="380px"
@@ -64,41 +99,65 @@ const TweetCard = ({ userId = 1 }) => {
       borderColor={cardBorderColor}
       bg={cardBgColor}
       overflow="hidden"
+      pos="relative"
     >
-      <Box p="20px">
-        <Box d="flex" alignItems="center">
-          <Box mr="3">
-            <Image
-              borderRadius="full"
-              boxSize="50px"
-              src="https://picsum.photos/200"
-              alt="User Avatar"
-            />
-          </Box>
+      <Box>
+        <Box pos="absolute" w="76px" h="22px" top="20px" left="20px">
+          <Image borderRadius="full" src={logo} alt="logo" />
+        </Box>
+        <Box
+          position="absolute"
+          left="50%"
+          transform="translateX(-50%)"
+          top="28px"
+          w="308px"
+          h="168px"
+        >
+          <Image borderRadius="full" src={bg} alt="bg" />
         </Box>
 
-        <Box mt="2" color="#EBD8FF">
-          <Text>{followers}</Text>
+        <Box sx={stripe}></Box>
+        <Box sx={avatarStyles}>
+          <Image src={userData.avatar} alt="User Avatar" />
         </Box>
+      </Box>
 
-        <Box d="flex" mt="2" alignItems="center">
-          <IconButton
+      <Box
+        textAlign="center"
+        pos="absolute"
+        bottom="0"
+        left="50%"
+        transform="translateX(-50%)"
+      >
+        {/* <IconButton
             variant="ghost"
             colorScheme="blue"
             aria-label="Like Tweet"
             icon={colorMode === 'light' ? <FaMoon /> : <FaSun />}
             onClick={toggleColorMode}
-          />
-          <Text fontSize="sm" color="gray.500" ml="2">
-            {userData ? userData.tweets : '-'}
+          /> */}
+        <Box sx={basicTweetCardStyles}>
+          <Text paddingBottom="16px" color="#EBD8FF">
+            {userData ? followers : '-'}
           </Text>
+          <Text color="#EBD8FF">{userData ? userData.tweets : '-'}</Text>
           <Button
+            sx={basicTweetCardStyles}
+            w="196px"
+            h="50px"
             ml="auto"
-            colorScheme="blue"
             onClick={handleFollowClick}
-            variant="outline"
+            variant="solid"
+            backgroundColor={
+              followers === userData.followers
+                ? 'cardButton.follow'
+                : 'cardButton.following'
+            }
+            boxShadow="0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)"
+            _hover={{ opacity: 0.8 }}
+            _focus={{ boxShadow: 'outline' }}
           >
-            {followers === userData.followers ? 'Following' : 'Follow'}
+            {followers === userData.followers ? 'Follow ' : 'Following'}
           </Button>
         </Box>
       </Box>
